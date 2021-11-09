b593eda2fd869e8e3547580569c769809548a108
Nathan LeRoy <NLeRoy917@gmail.com>
2021-11-09 09:55:41 -0500

    setup route dependencies

--- pephub/routers/version1.py
+++ pephub/routers/version1.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
 import peppy
 
 # fetch peps
@@ -6,8 +7,12 @@ from ..db import download_peps, load_data_tree
 download_peps()
 PEP_STORES = load_data_tree()
 
+# load dependencies
+from ..dependencies import *
+
 router = APIRouter(
-    prefix="/v1"
+    prefix="/v1",
+    dependencies=[Depends(verify_namespace), Depends(verify_project), Depends(validate_pep)]
 )
 
 @router.get("/")
@@ -16,47 +21,19 @@ async def root():
         "message": "welcome to the pepserver"
     }
 
-@router.get("/all")
+@router.get("/pep-list")
 async def return_all_peps():
     return PEP_STORES
 
 @router.get("/{namespace}")
 async def get_namespace(namespace: str):
-    if namespace not in PEP_STORES:
-        return {
-            "message": f"Namespace '{namespace}'' not found."
-        }
-    else:
-        return PEP_STORES[namespace]
+    return PEP_STORES[namespace]
 
 @router.get("/{namespace}/{pep_id}")
 async def get_pep(namespace: str, pep_id: str):
-
-    # validate namespace
-    if namespace not in PEP_STORES:
-        return {
-            "message": f"Namespace '{namespace}'' not found."
-        }
-
-    # validate pep_id
-    if pep_id not in PEP_STORES[namespace]:
-        return {
-            "message": f"PEP '{pep_id}'' not found."
-        }
-    
-    # validate pep
-    try:
-        proj = peppy.Project(PEP_STORES[namespace][pep_id])
-    except NotImplementedError as nie:
-        return {
-            "status": "error",
-            "message": "This PEP does not conform to the PEP 2.0 standards. You can read about those here: http://pep.databio.org/en/latest/specification/#sample-modifier-imply"
-        }
-
+    proj = peppy.Project(PEP_STORES[namespace][pep_id])
     return {
         "pep": proj
     }
-
-    
 
     
