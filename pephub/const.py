d3d8359a802927b426ec516c60f1fe71d2845e93
Nathan LeRoy <NLeRoy917@gmail.com>
2021-11-23 10:36:20 -0500

    misc

--- pephub/const.py
+++ pephub/const.py
@@ -2,6 +2,7 @@ PKG_NAME = "pephub"
 DATA_REPO = "https://github.com/pepkit/data.pephub.git"
 PEP_STORAGE_PATH = "/app/pephub/data"
 LOG_FORMAT = "%(levelname)s in %(funcName)s: %(message)s"
+
 DEFAULT_PORT = "80"
 
 TAGS_METADATA = [
