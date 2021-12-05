962454d06c7a7d77003f209cc22c7f88c0a10e99
Nathan LeRoy <NLeRoy917@gmail.com>
2021-12-05 16:15:13 -0500

    incorporate new template

--- pephub/routers/version1.py
+++ pephub/routers/version1.py
@@ -1,8 +1,11 @@
 import jinja2
 from fastapi import APIRouter, Depends
+from platform import python_version
 from starlette.responses import HTMLResponse
+from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 from ..const import BASE_TEMPLATES_PATH, STATICS_PATH
+import peppy
 
 # fetch peps
 from ..main import _PEP_STORES
@@ -17,9 +20,15 @@ router = APIRouter(
     tags=["root"]
 )
 
+ALL_VERSIONS = {
+    "peppy_version": peppy.__version__,
+    "python_version": python_version(),
+}
+
 @router.get("/")
-async def main():
-    return HTMLResponse(je.get_template("index.html").render())
+async def main(request: Request):
+    templ_vars = {"request": request}
+    return templates.TemplateResponse("index.html", dict(templ_vars, **ALL_VERSIONS))
 
 @router.get("/pep-list", summary="Return list of all available PEPs")
 async def return_all_peps():
