c936d962b729f929e10546cbf52464a2495af42b
Alip Arslan <aliparslan@outlook.com>
2022-08-05 10:51:29 -0400

    schema frontend

--- pephub/routers/eido.py
+++ pephub/routers/eido.py
@@ -3,8 +3,11 @@ import jinja2
 import os
 import peppy
 import shutil
+import aiofiles
+import requests
+import tempfile
 
-from fastapi import File, UploadFile
+from fastapi import File, UploadFile, Form
 from fastapi import APIRouter, Depends
 from peppy import __version__ as peppy_version
 from starlette.requests import Request
@@ -45,6 +48,22 @@ def vwrap(p, schema):
         x = str(e)
         print(x)
     return x
+
+def temp_pep(file):
+    """
+    Takes the name of a pep file and returns a pep
+    project object.
+
+    Uses a temporary directory so file is deleted from
+    server after use.
+    @param file name of file to be converted into peppy.Project
+    """
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        contents = file.read()
+        file_path = f"{tmpdirname}/{file.filename}"
+        with open(file_path, mode="wb") as f:
+            f.write(contents)
+        return peppy.Project(file_path)
 
 
 router = APIRouter(prefix="/eido", tags=["eido"])
@@ -90,7 +109,100 @@ async def status():
 #         )
 #     return JSONResponse(content=vals)
 
+@router.post("/validate/pep_and_schema")
+async def validate_both(pep: UploadFile = File(...), schema: UploadFile = File(...)):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        pep_contents = await pep.read()
+        pep_path = f"{tmpdirname}/{pep.filename}"
+        async with aiofiles.open(pep_path, mode="wb") as f:
+            await f.write(pep_contents)
+        project = peppy.Project(pep_path)
 
+        schema_contents = await schema.read()
+        schema_path = f"{tmpdirname}/{schema.filename}"
+        async with aiofiles.open(schema_path, mode="wb") as f:
+            await f.write(schema_contents)
+        schema = eido.read_schema(schema_path)
+
+        try:
+            response = eido.validate_project(
+                project, schema_path, exclude_case=True)
+        except Exception as e:
+            response = str(e)
+        if response is None:
+            response = "valid"
+
+        return {
+            "pep": project,
+            "schema": schema,
+            "response": response
+        }
+
+@router.get("/pep/{namespace}/{project}")
+async def pep_fromhub(namespace, project):
+    response = requests.get(
+        f"http://pephub.databio.org/pep/{namespace}{project}"
+    )
+    return {
+        "namespace": namespace,
+        "project": project,
+        "response": response.json() 
+    }
+
+@router.get("/schema/{namespace}/{project}", response_class=HTMLResponse)
+async def get_schema(request: Request, namespace: str, project: str):
+    # endpoint to schema.databio.org/...
+    # like pipelines/ProseqPEP.yaml
+
+    schema = eido.read_schema(
+        f"http://schema.databio.org/{namespace}/{project}")
+
+    return templates.TemplateResponse(
+        "schema.html",
+        {
+            "request": request,
+            "namespace": namespace,
+            "project": project,
+            "schema": schema,
+        },
+    )
+
+@router.post("/validate/pep/")
+async def validate_pep(
+    namespace: str = Form(), project: str = Form(), peps: List[UploadFile] = File(...)):
+    schema = f"http://schema.databio.org/{namespace}/{project}"
+    vals = {
+        "namespace": namespace,
+        "project": project,
+        "validations": [],
+    }
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        for pep in peps:
+            contents = await pep.read()
+            pep_path = f"{tmpdirname}/{pep.filename}"
+            async with aiofiles.open(pep_path, mode="wb") as f:
+                await f.write(contents)
+            pep_project = peppy.Project(pep_path)
+
+            try:
+                response = eido.validate_project(pep_project, schema, exclude_case=True)
+            except Exception as e:
+                response = []
+                for error in e.errors:
+                    response.append(f"{error.json_path}: {error.message}")
+            # if response is None:
+            #     response = "valid"
+            vals["validations"].append(
+                {
+                    "pep": pep.filename,
+                    "response": response
+                }
+            )
+    return JSONResponse(content=vals)
+
+
+
+# ! old /validate
 @router.post("/validate")
 async def validate_pep(
     request: Request,
@@ -139,13 +251,16 @@ async def validate_pep(
     # return HTMLResponse(je.get_template("validation_results.html").render(**vals))
 
 
+@router.get("/")
+async def main():
+    print(je.list_templates())
+    return HTMLResponse(je.get_template("hello.html").render())
+
 # @router.get("/")
 # async def main():
-#     print(je.list_templates())
-#     return HTMLResponse(je.get_template("index.html").render())
-
+#     return HTMLResponse(je.get_template("schema.html").render())
 
 @router.get("/validator")
 async def main():
     print(je.list_templates())
-    return FileResponse(os.path.join(STATICS_PATH, "index.html"))
+    return FileResponse(os.path.join(STATICS_PATH, "index2.html"))
