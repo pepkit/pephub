2cd6fb41f220824867c6c71e944602730cda1b10
Nathan LeRoy <NLeRoy917@gmail.com>
2023-06-09 16:50:37 -0400

    changelog

--- web/src/components/forms/components/shema-tag.tsx
+++ web/src/components/forms/components/shema-tag.tsx
@@ -8,7 +8,7 @@ export const SchemaTag: FC<SchemaTagProps> = ({ schema }) => {
   return (
     <div>
       <small className="d-flex flex-row align-items-center shadow-sm mb-2 px-2 py-1 fw-semibold text-primary bg-primary bg-opacity-10 border border-primary border-opacity-10 rounded-2">
-        <span className="me-1">{schema || 'Undefined'}</span>
+        <span className="me-1">{schema || 'No Schema'}</span>
       </small>
     </div>
   );
