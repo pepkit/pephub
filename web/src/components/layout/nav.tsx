fd28ab58f99ee83092ae0bf5feeb30c7b0ad9a01
Khoroshevskyi <sasha99250@gmail.com>
2023-07-19 10:41:39 -0400

    Added about page

--- web/src/components/layout/nav.tsx
+++ web/src/components/layout/nav.tsx
@@ -183,6 +183,11 @@ export const Nav: FC = () => {
               </a>
             </li>
             <li className="text-body mx-2 my-0 nav-item h5 pt-1">
+              <a className="nav-link" href="/about">
+                <i className="bi bi-info-circle me-1"></i>About
+              </a>
+            </li>
+            <li className="text-body mx-2 my-0 nav-item h5 pt-1">
               {user ? (
                 <div className="mx-2 my-0 nav-item h5 pt-1">
                   <Dropdown className="me-3">
