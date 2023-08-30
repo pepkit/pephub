e6f2e44797d7e62c982c37a12b63adba4b94e84f
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-30 12:35:24 -0400

    update opengraph image

--- web/src/components/layout/seo.tsx
+++ web/src/components/layout/seo.tsx
@@ -11,7 +11,7 @@ export const SEO: FC<Props> = ({ title, description, image }) => {
   const pageTitle = title || 'PEPhub';
   const pageDescription =
     description || 'PEPhub is a web interface, API, and database to store and manage biological metadata.';
-  const pageImage = image || '/landing-meta.png';
+  const pageImage = image || '/og-image.png';
   const pageUrl = 'https://pephub.databio.org/';
   const pageType = 'website';
   return (
