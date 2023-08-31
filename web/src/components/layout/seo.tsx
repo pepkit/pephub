53477b1300961275baacd77217339adc0a2a58a4
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-31 11:14:50 -0400

    add support for twitter cards too

--- web/src/components/layout/seo.tsx
+++ web/src/components/layout/seo.tsx
@@ -16,13 +16,26 @@ export const SEO: FC<Props> = ({ title, description, image }) => {
   const pageType = 'website';
   return (
     <Helmet>
+      {/*  general */}
       <title>{pageTitle}</title>
       <meta name="description" content={pageDescription} />
-      <meta property="og:description" content={pageDescription} />
-      <meta property="og:title" content={pageTitle} />
-      <meta property="og:image" content={pageImage} />
+      <meta name="author" content="databio" />
+
+      {/*  open graph */}
+      <meta property="og:type" content={pageType} />
+      <meta name="description" property="og:description" content={pageDescription} />
+      <meta name="title" property="og:title" content={pageTitle} />
+      <meta name="image" property="og:image" content={pageImage} />
       <meta property="og:type" content={pageType} />
       <meta property="og:url" content={pageUrl} />
+
+      {/*  twitter card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@shefflab" />
+      <meta name="twitter:creator" content="@shefflab" />
+      <meta name="twitter:title" content={pageTitle} />
+      <meta name="twitter:description" content={pageDescription} />
+      <meta name="twitter:image" content={pageImage} />
     </Helmet>
   );
 };
