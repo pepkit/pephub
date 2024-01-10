7d68bb7449183cfb0f1c2a822cde352b24aea748
Nathan LeRoy <NLeRoy917@gmail.com>
2024-01-10 13:59:38 -0500

    padding to pop interface

--- web/src/components/pop/pop-interface.tsx
+++ web/src/components/pop/pop-interface.tsx
@@ -43,89 +43,91 @@ export const PopInterface = ({ project }: Props) => {
   } else {
     return (
       <Fragment>
-        <div className="mt-3 border-top w-100"></div>
         <div className="px-2">
-          <div className="d-flex flex-column">
-            {allProjectsInfo?.results.map((project: ProjectAnnotation | null) =>
-              project === null ? null : (
-                <PopCard
-                  currentPeps={peps?.items || []}
-                  key={`${project.namespace}/${project.name}:${project.tag}`}
-                  project={project}
-                />
-              ),
-            )}
-          </div>
-          <div className="d-flex flex-row align-items-center justify-content-center mt-3">
-            {!isAddingNew ? (
-              <button className="btn btn-success" onClick={() => setIsAddingNew(true)}>
-                <i className="bi bi-plus-circle me-1"></i>
-                Add
-              </button>
-            ) : (
-              <div className="d-flex flex-column w-100 align-items-center mb-5">
-                <div className="border-top mt-3 w-100"></div>
-                <div
-                  className="bg-white"
-                  style={{
-                    width: '10%',
-                    transform: 'translateY(-30%)',
-                  }}
-                >
-                  <p className="text-secondary text-sm text-center">
-                    <i className="bi bi-plus-circle me-1"></i>
-                    Add a new PEP
-                  </p>
-                </div>
-                <div className="d-flex flex-row align-items-center gap-2 w-100">
-                  <div className="d-flex flex-row align-items-center w-100">
-                    <div className="w-25 me-1">
-                      <NamespaceSearchDropdown
-                        value={addToPopNamespace}
-                        onChange={(value) => setAddToPopNamespace(value)}
-                      />
-                    </div>
-                    <div className="w-75">
-                      <PepSearchDropdown
-                        value={addToPopRegistry}
-                        onChange={(value) => setAddToPopRegistry(value)}
-                        namespace={addToPopNamespace}
-                      />
-                    </div>
-                  </div>
-                  <button
-                    className="btn btn-md btn-success"
-                    onClick={() => {
-                      let newPeps = peps?.items || [];
-                      const [newNamespace, newPojectNameAndTag] = addToPopRegistry.split('/');
-                      const [newProjectName, newProjectTag] = newPojectNameAndTag.split(':');
-                      newPeps?.push({
-                        sample_name: `${newNamespace}/${newProjectName}:${newProjectTag}`,
-                        namespace: newNamespace,
-                        name: newProjectName,
-                        tag: newProjectTag,
-                      });
-                      sampleTableMutation.mutate(newPeps);
+          <div className="mt-3 border-top w-100"></div>
+          <div className="px-2">
+            <div className="d-flex flex-column">
+              {allProjectsInfo?.results.map((project: ProjectAnnotation | null) =>
+                project === null ? null : (
+                  <PopCard
+                    currentPeps={peps?.items || []}
+                    key={`${project.namespace}/${project.name}:${project.tag}`}
+                    project={project}
+                  />
+                ),
+              )}
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center my-3">
+              {!isAddingNew ? (
+                <button className="btn btn-success" onClick={() => setIsAddingNew(true)}>
+                  <i className="bi bi-plus-circle me-1"></i>
+                  Add
+                </button>
+              ) : (
+                <div className="d-flex flex-column w-100 align-items-center mb-5">
+                  <div className="border-top mt-3 w-100"></div>
+                  <div
+                    className="bg-white"
+                    style={{
+                      width: '10%',
+                      transform: 'translateY(-30%)',
                     }}
-                    disabled={addToPopNamespace === '' || addToPopRegistry === '' || sampleTableMutation.isPending}
                   >
-                    {sampleTableMutation.isPending ? (
-                      <Fragment>
-                        <span className="d-flex flex-row align-items-center">
-                          <LoadingSpinner className="w-4 h-4 spin me-1 fill-light" />
-                          Add
-                        </span>
-                      </Fragment>
-                    ) : (
-                      <Fragment>Add</Fragment>
-                    )}
-                  </button>
-                  <button className="btn btn-md btn-outline-dark" onClick={() => setIsAddingNew(false)}>
-                    Cancel
-                  </button>
+                    <p className="text-secondary text-sm text-center">
+                      <i className="bi bi-plus-circle me-1"></i>
+                      Add a new PEP
+                    </p>
+                  </div>
+                  <div className="d-flex flex-row align-items-center gap-2 w-100">
+                    <div className="d-flex flex-row align-items-center w-100">
+                      <div className="w-25 me-1">
+                        <NamespaceSearchDropdown
+                          value={addToPopNamespace}
+                          onChange={(value) => setAddToPopNamespace(value)}
+                        />
+                      </div>
+                      <div className="w-75">
+                        <PepSearchDropdown
+                          value={addToPopRegistry}
+                          onChange={(value) => setAddToPopRegistry(value)}
+                          namespace={addToPopNamespace}
+                        />
+                      </div>
+                    </div>
+                    <button
+                      className="btn btn-md btn-success"
+                      onClick={() => {
+                        let newPeps = peps?.items || [];
+                        const [newNamespace, newPojectNameAndTag] = addToPopRegistry.split('/');
+                        const [newProjectName, newProjectTag] = newPojectNameAndTag.split(':');
+                        newPeps?.push({
+                          sample_name: `${newNamespace}/${newProjectName}:${newProjectTag}`,
+                          namespace: newNamespace,
+                          name: newProjectName,
+                          tag: newProjectTag,
+                        });
+                        sampleTableMutation.mutate(newPeps);
+                      }}
+                      disabled={addToPopNamespace === '' || addToPopRegistry === '' || sampleTableMutation.isPending}
+                    >
+                      {sampleTableMutation.isPending ? (
+                        <Fragment>
+                          <span className="d-flex flex-row align-items-center">
+                            <LoadingSpinner className="w-4 h-4 spin me-1 fill-light" />
+                            Add
+                          </span>
+                        </Fragment>
+                      ) : (
+                        <Fragment>Add</Fragment>
+                      )}
+                    </button>
+                    <button className="btn btn-md btn-outline-dark" onClick={() => setIsAddingNew(false)}>
+                      Cancel
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </Fragment>
