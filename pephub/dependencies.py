c6ff0d28cd887f2f7dddf6a34c7fd158a9d83be5
Khoroshevskyi <sasha99250@gmail.com>
2024-06-27 13:21:26 -0400

    changed get_project args

--- pephub/dependencies.py
+++ pephub/dependencies.py
@@ -197,7 +197,7 @@ def get_project(
     project: str,
     tag: Optional[str] = DEFAULT_TAG,
     agent: PEPDatabaseAgent = Depends(get_db),
-    with_ids: Optional[bool] = True,  # TODO: change it to false
+    with_ids: Optional[bool] = False,
 ) -> Dict[str, Any]:
     try:
         proj = agent.project.get(namespace, project, tag, raw=True, with_id=with_ids)
