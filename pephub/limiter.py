e47842dffc3539c9ef650b2fd4e678b4afb2a075
Khoroshevskyi <sasha99250@gmail.com>
2026-01-26 23:10:51 -0500

    lint

--- pephub/limiter.py
+++ pephub/limiter.py
@@ -3,7 +3,6 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-
 limiter = Limiter(key_func=get_remote_address)
 
 
